75a19070ae4649f2e0424808741914be30f7cd35
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2025-12-04 23:43:46 +0530

    responsive changes & firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWBoT3XqAULWHD9xdBdzKloYmWE1PJTxM",
+  authDomain: "mymovies-8.firebaseapp.com",
+  projectId: "mymovies-8",
+  storageBucket: "mymovies-8.firebasestorage.app",
+  messagingSenderId: "401812098894",
+  appId: "1:401812098894:web:acc63a7df24cd50392d81f"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+     toast.error(error.code.split('/')[1].split('-').join(" "));
+  }
+};
+
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split('/')[1].split('-').join(" "));
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, login, signup, logout };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,23 +2,50 @@ import React from 'react'
 import { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
-
+import { login,signup } from '../../firebase'
+import mymovies_spinner from '../../assets/mymovies_spinner.gif'
 
 const Login = () => {
   
-  const[signState,setSignState]= useState("Sign In")    
+  const[signState,setSignState]= useState("Sign In");
+  const [name, setName]= useState(""); 
+  const [email, setEmail]= useState(""); 
+  const [password, setPassword]= useState(""); 
+  const [loading,setLoading]= useState(false); //initial loading state false;
+
+  const user_auth = async(event)=>{
+    event.preventDefault();
+    setLoading(true);
+    if(signState=="Sign In"){
+      await login(email,password);
+    }
+    else{
+      await signup(name,email,password);
+    }
+    setLoading(false);
+  }
 
   return (
+    loading?<div className="login-spinner">
+      <img src={mymovies_spinner} alt="Loading..." />
+    </div>:
     <div className='login'>
       <img src={logo} alt="" className='login-logo'/>
       <div className='login-form'>
         <h1>{signState}</h1>
+
         <form action="">
-          {signState=="Sign Up" ?<input type="text" placeholder='Your name' />: <></>}
+          {signState=="Sign Up" ?
+          <input value={name} onChange={(e)=>{setName(e.target.value)}} 
+          type="text" placeholder='Your name' />: <></>}
           
-          <input type="email" placeholder='Your Email' />
-          <input type="password" placeholder='Password' />
-          <button>{signState}</button>
+          <input value={email} onChange={(e)=>{setEmail(e.target.value)}} 
+          type="email" placeholder='Your Email' />
+
+          <input value={password} onChange={(e)=>{setPassword(e.target.value)}} 
+          type="password" placeholder='Password' />
+
+          <button onClick={user_auth} type='submit'>{signState}</button>
            <div className="form-help">
             <div className="remember">
               <input type='checkbox'/>
@@ -32,7 +59,6 @@ const Login = () => {
           <p>Already have account? <span onClick={()=> {setSignState("Sign In")}} >Sign In Now</span></p> } 
         </div>
       </div>
-      
     </div>
   )
 }
